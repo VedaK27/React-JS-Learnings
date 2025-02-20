7ca7b6d34960f4fa5fa3e1ff24e722866a02cc4e
VedaK27 <vedakimbahune27@gmail.com>
2025-02-20 15:18:35 +0530

    Delete a blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,14 +10,21 @@ function App() {
     { title : "Welcome Partu!" , body: "lore eispum...." , author:"mario" , id:2} ,
     { title : "Web dev top tips" , body:"lorem ispum..." , author:"ysodhi" , id:3}
   ]) ;
+
+  const [name , setName] = useState("Veda")
+  const [rel , setRel] = useState("Girlfriend")
   const handledelete =(id) => {
         const newBlogs = blogs.filter(blog => blog.id !=id) ;
         setBlogs(newBlogs) ;
   }
+  useEffect(() =>{
+    console.log("Use effect ran!") ;
+  },[name]);
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs!" handledelete={handledelete}/>
-
+      <button onClick={()=>setName("Rucha")}>OnClick</button>
+      <p>{name}</p>
     </div>
       
   )
